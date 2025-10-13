6e03ccb82e5feb6c52af29c908fd86849bc9da3e
cboy.chinedu <cboy.chinedu@gmail.com>
2025-10-13 23:01:22 +0100

    learnt do-while loops

---
+++ dowhileLoops.java
@@ -0,0 +1,18 @@
+public class dowhileLoops {
+    // Runing the main function 
+    public static void main(String[] args) {
+        // Setting the count value to zero 
+        int count = 0; 
+
+        // do-while loop 
+        do { 
+            // Setting the count 
+            System.out.println("Counting " + count); 
+
+            // increment 
+            count++; 
+
+          // While loop   
+        } while (count <= 20);
+    }
+}
---
+++ whileLoops.java
@@ -0,0 +1,17 @@
+public class whileLoops {
+    // Running the main function 
+    public static void main(String[] args) {
+        // Setting the count 
+        int count = 0;
+        int stop = 100;  
+
+        // Executing the while loop 
+        while (count <= stop) {
+            // Display the count 
+            System.out.printf("Counting %d", count).println(); 
+
+            // Increment 
+            count++; 
+        }
+    }
+}
